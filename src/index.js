8b4239c0948236adeee836ee2b9a0b85d45cde8b
DeadNord <eosipopo@gmail.com>
2021-09-23 21:37:16 +0300

    v2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+// Import
+import main from "./main.hbs";
+import menu from "./menu.json";
+
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
@@ -7,41 +11,42 @@ const Theme = {
 
 const refs = {
     toogle: document.getElementById(`theme-switch-toggle`),
-    body: document.body
+    body: document.querySelector("body"),
+    cards: document.querySelector(".menu"),
+    card: main(menu)
 };
 
-// Listener
+
+// Cards Insert
+refs.cards.insertAdjacentHTML("beforeend", refs.card);
+
+
+Listener
 refs.toogle.addEventListener("change", switchTheme);
 
-function switchTheme() { }
-
-// ffffffff
-
-$('body').on('change', function(){ // при выборе темы..
-  let val = $(this).val(); // получим в переменную атрибут value
-  $('body') // обратимся к body
-    .attr('class', val+'theme'); // добавим выбранную тему с префиксом 'theme-'
+function switchTheme() {
+    $('body').on('change', function () { // при выборе темы..
+        let val = $(this).val(); // получим в переменную атрибут value
+        $('body') // обратимся к body
+            .attr('class', val + 'theme'); // добавим выбранную тему с префиксом 'theme-'
     
-  // Далее сохраняем значение в localStorage
-  localStorage.setItem(val, 'theme');
-  // Но я её закоментил, ибо в сниппете она работать не будет.
-});
+        // Далее сохраняем значение в localStorage
+        localStorage.setItem(val, 'theme');
+        // Но я её закоментил, ибо в сниппете она работать не будет.
+    });
 
-// Сохранение мы написали, но если обновить страницу, то дизайн слетит, по этому пишем ещё код, который будет брать значение из локального хранилища и ставить его..
-$(document).on('ready', function(){
-  let theme = localStorage('theme') || 'light'; // Получаем значение из хранилища, если его нет, то используем 'light' как дефолтное.
-  // P.s. это код тоже не будет работать в сниппете.
-  // А дальше как и выше
-  $('body') // обращаемся к body
-    .attr('class', theme+'-theme'); // ставим класс с префиксом.
+    // Сохранение мы написали, но если обновить страницу, то дизайн слетит, по этому пишем ещё код, который будет брать значение из локального хранилища и ставить его..
+    $(document).on('ready', function () {
+        let theme = localStorage('theme') || 'light'; // Получаем значение из хранилища, если его нет, то используем 'light' как дефолтное.
+        // P.s. это код тоже не будет работать в сниппете.
+        // А дальше как и выше
+        $('body') // обращаемся к body
+            .attr('class', theme + '-theme'); // ставим класс с префиксом.
     
-  // ещё для красоты можно сделать так
-  $('#theme option:selected') // обратимся к селектору с темой
-    .prop('selected', false); // у "выбранного" уберём  "выбор"
-  $('#theme option[value="'+theme+'"]') // найдём так же тот option с сохранённой темой
-    .prop('selected', true); // и поставим "выбор" на него.
-  
-});
-
-// let source = $("#entry-template").html();
-// let template = Handlebars.compile(source);
+        // ещё для красоты можно сделать так
+        $('#theme option:selected') // обратимся к селектору с темой
+            .prop('selected', false); // у "выбранного" уберём  "выбор"
+        $('#theme option[value="' + theme + '"]') // найдём так же тот option с сохранённой темой
+            .prop('selected', true); // и поставим "выбор" на него.
+    });
+}
